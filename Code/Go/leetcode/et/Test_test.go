13562f14824394e32f8763c809d424ecf72b1903
haifei.zhang <exfly@outlook.com>
2018-07-20 10:26:11 +0800

    update: add some comments

--- Code/Go/leetcode/et/Test_test.go
+++ Code/Go/leetcode/et/Test_test.go
@@ -6,13 +6,23 @@ import (
 )
 
 func TestGoSynax(t *testing.T) {
+
+	// make slice
 	a := make([]int, 3)
 	t.Error(reflect.TypeOf(a))
 	sp := make([]int, 10)
 	t.Error(sp)
+
+	// reflect
 	t.Error(reflect.TypeOf(sp))
 	sp = append(sp, 10)
 	t.Error(sp)
 	sp[0] = 1
 	t.Error(sp)
+
+	// int div
+	t.Error(3 / 2)
+	t.Error(3 / 2.0)
+	t.Error(3.0 / 2)
+
 }
