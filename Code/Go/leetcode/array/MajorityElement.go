1147a35db7a2815d1d3d696bec6311b5f72efda9
haifei.zhang <exfly@outlook.com>
2018-07-20 09:54:18 +0800

    update: fix space  / ++

--- Code/Go/leetcode/array/MajorityElement.go
+++ Code/Go/leetcode/array/MajorityElement.go
@@ -4,9 +4,9 @@ func majorityElement(nums []int) int {
 	mp := make(map[int]int)
 	l := len(nums)
 	var result int
-	mid := l/2
+	mid := l / 2
 	for _, i := range nums {
-		mp[i] += 1
+		mp[i]++
 		if mp[i] > mid {
 			result = i
 			return result
