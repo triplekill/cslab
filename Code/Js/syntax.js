6e3ba2b73a0b3a99b15fcf99fcbb92437ea5c95e
haifei.zhang <haifei.zhang@chaitin.com>
2018-10-13 09:44:59 +0800

    feat: js synax

--- Code/Js/syntax.js
+++ Code/Js/syntax.js
@@ -30,6 +30,18 @@ for (var i = 0; i < 2; i++) {
   console.log("for i:" + i);
 }
 
+var obj = {
+  foo: "foov",
+  bar: "bar",
+  getFoo: function() {
+    return this.foo;
+  }
+};
+for (var i in obj) {
+  console.log("键名：", i);
+  console.log("键值：", obj[i]);
+}
+
 do {
   console.log(i);
   i++;
@@ -45,3 +57,56 @@ function b64Decode(str) {
 
 console.log(b64Encode("你好")); // "JUU0JUJEJUEwJUU1JUE1JUJE"
 console.log(b64Decode("JUU0JUJEJUEwJUU1JUE1JUJE")); // "你好"
+
+with (document.links[0]) {
+  console.log(href);
+  console.log(title);
+  console.log(style);
+}
+
+function f(x) {
+  if (x instanceof Function) {
+    return x.name;
+  }
+  console.log(arguments);
+  return "";
+}
+console.log(f(f));
+
+function throwit() {
+  throw new Error("");
+}
+function catchit() {
+  try {
+    throwit();
+  } catch (e) {
+    console.log(e.message); // print stack trace
+  } finally {
+    console.log("完成清理工作");
+  }
+}
+catchit();
+
+function UserError(message) {
+  this.message = message || "默认信息";
+  this.name = "UserError";
+}
+
+UserError.prototype = new Error();
+UserError.prototype.constructor = UserError;
+
+function Animal(name) {
+  this.name = name;
+}
+Animal.prototype.color = "white";
+
+var cat1 = new Animal("大毛");
+var cat2 = new Animal("二毛");
+
+cat1.color; // 'white'
+cat2.color; // 'white'
+cat1.color = "black";
+
+cat1.color; // 'black'
+cat2.color; // 'yellow'
+Animal.prototype.color; // 'yellow';
