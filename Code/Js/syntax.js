4661058842fc87392b51ce48f9137e77b58b0a22
haifei.zhang <haifei.zhang@chaitin.com>
2018-10-16 16:51:55 +0800

    feat: js

--- Code/Js/syntax.js
+++ Code/Js/syntax.js
@@ -110,3 +110,27 @@ cat1.color = "black";
 cat1.color; // 'black'
 cat2.color; // 'yellow'
 Animal.prototype.color; // 'yellow';
+
+function PubSub() {
+  let yourMsg = {};
+  yourMsg.peopleList = [];
+  yourMsg.listen = function(fn) {
+    this.peopleList.push(fn);
+  };
+  yourMsg.triger = function() {
+    for (var i = 0, fn; (fn = this.peopleList[i++]); ) {
+      fn.apply(this, arguments);
+    }
+  };
+
+  yourMsg.listen(function(name) {
+    console.log(`${name}收到了你的消息`);
+  });
+  yourMsg.listen(function(name) {
+    console.log("哈哈");
+  });
+
+  yourMsg.triger("张三");
+  yourMsg.triger("李四");
+}
+PubSub();
