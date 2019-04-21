581c8a5e9e38c4f9d8fa16b18af0ed548da5d9c8
haifei.zhang <haifei.zhang@chaitin.com>
2019-04-21 16:03:21 +0800

    del go session

---
+++ Code/Go/lngo/rxgo/go.mod
@@ -0,0 +1,8 @@
+module github.com/exfly/cslab/Code/Go/lngo/rxgo
+
+go 1.12
+
+require (
+	github.com/reactivex/rxgo v1.0.0
+	github.com/stretchr/testify v1.3.0 // indirect
+)
---
+++ Code/Go/lngo/rxgo/t_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/reactivex/rxgo/iterable"
+	"github.com/reactivex/rxgo/observable"
+	"github.com/reactivex/rxgo/observer"
+)
+
+func Test_main(t *testing.T) {
+	watcher := observer.Observer{
+		// Register a handler function for every next available item.
+		NextHandler: func(item interface{}) {
+			fmt.Printf("Processing: %v\n", item)
+		},
+
+		// Register a handler for any emitted error.
+		ErrHandler: func(err error) {
+			fmt.Printf("Encountered error: %v\n", err)
+		},
+
+		// Register a handler when a stream is completed.
+		DoneHandler: func() {
+			fmt.Println("Done!")
+		},
+	}
+
+	it, _ := iterable.New([]interface{}{1, 2, 3, 4, errors.New("bang"), 5})
+	source := observable.From(it)
+	sub := source.Subscribe(watcher)
+
+	// wait for the channel to emit a Subscription
+	t.Log(<-sub)
+}
